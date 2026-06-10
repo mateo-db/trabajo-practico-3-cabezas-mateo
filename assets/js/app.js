953c66f2e621f8602cd41f7f4893b500aba41965
mateo-db <cabezasluciano752@gmail.com>
2026-06-10 01:21:02 -0300

    feat: se guardó info de API en arreglo y se implementó renderizado de cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,17 +16,39 @@ const traerPersonajes = async () => {
     try {
         const dataCrudaPj = await fetch(urlApiGeneral)
         const dataJson = await dataCrudaPj.json()
-
-        return dataJson.results.slice(0, 11)
+        return dataJson.results
     } catch (error) {
         console.error(error)
     }
 
 }
 
-// const inApp = async () => {
-//     const listRec = await traerPersonajes()
+//variable con funcion asincronica que guarda el resultado de "traerPersonajes" en el arreglo de personajes
+const personajesCargados = async () => {
+    personajes = await traerPersonajes()
+    // console.log(personajes)
+    cargarPersonajes(personajes)
+}
 
-//     console.log(listRec)
-// }
+// personajesCargados()
+//se declara variable que utilizara una funcion con parametro generico para recorrer el arreglo que le pasemos y renderizar las cards
+const cargarPersonajes = (array) => {
+    rowCards.innerHTML = ""
+    array.forEach((personaje) => {
+        rowCards.innerHTML +=`
+            <div class="col-2 mt-4 mb-4">
+                <div class="card" style="width: 10rem;">
+                    <img src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}" class="card-img-top" style="height: 250px; object-fit: cover" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${personaje.name}</h5>
+                        <h6 class="card-text">${personaje.occupation}</h6>
+                        <h6 class="card-text">${personaje.status}</h6>
+                        <a href="#" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalPjSimpson" data-id="${personaje.id}">Ver detalle</a>
+                    </div>
+                </div>
+            </div>
+        `
+    })
+}
 
+personajesCargados()
